ce1943a2453cd1dfaa12cad1c9734adf5a4ebc2c
Hyun Seo <hyunkseo@icloud.com>
2021-04-01 13:27:31 -0400

    Added missing sign parameter in webworker

--- src/webworker/custom.d.ts
+++ src/webworker/custom.d.ts
@@ -3,7 +3,7 @@ declare module 'comlink-loader!*' {
   class WebpackWorker extends WebWorker {
     constructor();
     decryptString(data: string, pw: string): Promise<string>;
-    encryptString(data: string, pw: string): Promise<string>;
+    encryptString(data: string, pw: string, sign: boolean): Promise<string>;
     decryptAttachment(data: ProcessedData, pw: string): Promise<ProcessedData>;
     encryptAttachment(data: ProcessedData, pw: string): Promise<ProcessedData>;
   }
